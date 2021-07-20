901d66336983e78bcd27e62b48877adad6e601f3
macbook extdev <test>
2021-07-20 10:22:22 +0500

    remove podfile. adding spm

--- Pods/Target Support Files/Pods-SimpleRest-SimpleRestUITests/Pods-SimpleRest-SimpleRestUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimpleRest_SimpleRestUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SimpleRest_SimpleRestUITests
-@end
--- Pods/Target Support Files/Pods-SimpleRestTests/Pods-SimpleRestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimpleRestTests : NSObject
-@end
-@implementation PodsDummy_Pods_SimpleRestTests
-@end
